ce96a8269a1fae8e31cf0179f21605d7d5956ee0
TrueAbby24 <abby.alyssa@gmail.com>
2018-06-26 16:35:20 +1000

    csv error check

--- async.js
+++ async.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  var serverURL = "";
 // xmlhttpRequest objects setup
   var uploadRequest, dataRequest;
   if(window.XMLHttpRequest){
@@ -10,7 +11,7 @@ $(document).ready(function(){
   }
   // handle server responses
   uploadRequest.onreadystatechange=function(){
-    if(uploadRequest.readyState==4 && uploadRequest.status==200) {
+    if(uploadRequest.readyState == 4 && uploadRequest.status == 200) {
       // document.getElementById("myDiv").innerHTML = uploadRequest.responseText;
       console.console.log("upload response:");
       console.log(uploadRequest.responseText);
@@ -51,25 +52,30 @@ $(document).ready(function(){
     var csv = event.target.result;
     var txt = csv.split(/\r\n|\n/);
     var headers = txt[0].split(", ");
-    if (headers.length == 2 && !(headers[0] === "") && !(headers[1] === "")) {
-      var result = [];
-      for (var i=1; i < txt.length; i++) {
-        var data = txt[i].split(',');
-        if(data.length != 2 || data[0] === "" || data[1] === "") {
-          alert("Invalid csv format!");
-          exit();
+    var validCsv = true;
+    if (headers.length == 2 && headers[0] == "name"
+      && headers[1] == "address") {
+        var result = [];
+        for (var i=1; i < txt.length; i++) {
+          var data = txt[i].split(',"');
+          if(data.length != 2 || data[0] === "" || data[1] === "") {
+            alert("Invalid csv format!\n");
+            validCsv = false;
+            break;
+          }
+          var entry = {};
+          entry[headers[0]] = data[0];
+          entry[headers[1]] = data[1].replace('"','');
+          result.push(entry);
         }
-        var entry = {};
-        entry[headers[0]] = data[0];
-        entry[headers[1]] = data[1].replace('"','');
-        result.push(entry);
-      }
-      console.log(JSON.stringify(result));
-      dataRequest.open("POST", "http://localhost:80/upload.php", true);
-      dataRequest.setRequestHeader("data", JSON.stringify(result));
-      dataRequest.send();
+        if(validCsv) {
+          console.log(JSON.stringify(result));
+          dataRequest.open("POST",serverURL + "upload.php", true);
+          dataRequest.setRequestHeader("data", JSON.stringify(result));
+          dataRequest.send();
+        }
     } else {
-      alert("Invalid headers");
+        alert("Invalid headers");
     }
   }
 
